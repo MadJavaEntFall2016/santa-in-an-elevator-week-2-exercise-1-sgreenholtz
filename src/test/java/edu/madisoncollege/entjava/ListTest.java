458089c9defa96e77fb1f827f00656acedb2cb29
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-09-10 13:58:24 -0500

    Test cases added for Lists

--- src/test/java/edu/madisoncollege/entjava/ListTest.java
+++ src/test/java/edu/madisoncollege/entjava/ListTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -36,6 +37,30 @@ public class ListTest {
         myList.add(elementToInsert);
         assertEquals("List size is incorrect", 4, myList.size());
         assertTrue("List missing inserted element", myList.contains(elementToInsert));
+    }
+
+    @Test
+    public void testRemove() {
+        int indexToRemove = 0;
+        String itemToRemove = "Item 1";
+        myList.remove(indexToRemove);
+        assertEquals("List size is incorrect", 2, myList.size());
+        assertFalse("Item not removed", myList.contains(itemToRemove));
+    }
+
+    @Test
+    public void testGet() {
+        String itemToGet = "Item 2";
+        int index = 1;
+        String result = myList.get(index);
+        assertEquals("Item not retrieved successfully", itemToGet, result);
+
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIndexOutOfBounds() {
+        int index = 5;
+        myList.get(index);
     }
 
 
